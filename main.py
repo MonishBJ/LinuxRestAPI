16fbe7174a24db85c5904d79c7959492ef2e64d0
Vilas Gowda <vilasgowda65@gmail.com>
2023-02-16 22:34:43 +0530

    day1

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import jsonify, Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+
+api = Api(app)
+
+
+class Hello(Resource):
+
+    def get(self):
+        return jsonify({'data': 'welcome to demo'})
+
+    def post(self):
+        return jsonify({'data': "vials"}), 201
+
+api.add_resource(Hello,'/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
